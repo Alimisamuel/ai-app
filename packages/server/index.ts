3ecb10f0cd74939d8812b561e210ce66f83bf8e5
Samuel <50087054+Alimisamuel@users.noreply.github.com>
2025-12-12 13:52:23 +0100

    set up prettier and shadcn ui

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,20 @@
 import express from 'express';
 import type { Request, Response } from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req:Request,res:Response)=>{
-    res.send("Hello word!!")
-})
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello word!!');
+});
 
-app.get('/api/hello', (req:Request,res:Response)=>{
-    res.send({message:"Hello word!!"})
-})
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.send({ message: 'Hello word!!' });
+});
 
-app.listen(port, ()=>{
-    console.log(`server is running on http://localhost:${port}`)
-})
+app.listen(port, () => {
+   console.log(`server is running on http://localhost:${port}`);
+});
