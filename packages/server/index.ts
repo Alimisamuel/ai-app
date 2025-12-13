1712950678bae821c5fde20849919667ba447fc7
Samuel <50087054+Alimisamuel@users.noreply.github.com>
2025-12-13 14:43:17 +0100

    server done

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,38 +1,26 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
+import { chatController } from './controllers/chat.controller';
+
 
 dotenv.config();
 
-const client = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-})
-
 const app = express();
-app.use(express.json())
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
    res.send('Hello word!!');
 });
 
+
+
 app.get('/api/hello', (req: Request, res: Response) => {
    res.send({ message: 'Hello word!!' });
 });
 
-app.post('api/chat', async (req:Request, res:Response) => {
-    const {prompt} = req.body;
-
-    const response = await client.responses.create({
-        model:'gpt-4o-mini',
-        input:prompt,
-        temperature:0.2,
-        max_output_tokens:100
-    });
-
-    res.json({message:response.output_text})
-})
+app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
    console.log(`server is running on http://localhost:${port}`);
---
+++ packages/server/services/chat.service.ts
@@ -0,0 +1,33 @@
+import OpenAI from 'openai';
+import { conversationRepository } from '../repositories/conversation.repository';
+
+const { getLastResponseId, setLastResponseId } = conversationRepository;
+
+const client = new OpenAI({
+   apiKey:
+      '',
+});
+
+type ChatResponse = {
+id:string;
+message:string;
+}
+export const chatService = {
+   async sendMessage(prompt: string, conversationId: string):Promise<ChatResponse> {
+      const response = await client.responses.create({
+         model: 'gpt-4o-mini',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 100,
+         previous_response_id: getLastResponseId(conversationId),
+      });
+
+      setLastResponseId(conversationId, response.id);
+        return {
+            id:response.id,
+            message:response.output_text
+        };
+   },
+
+ 
+};
